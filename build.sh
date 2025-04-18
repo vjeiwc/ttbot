752f47fa8e912eb600b02a001db1c0d583a9f697
vjeiwc <value-jogging-icon@duck.com>
2025-04-18 17:28:19 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+set -e # Остановить при ошибках
+
+# Установка FFmpeg
 sudo apt-get update
 sudo apt-get install -y ffmpeg
+
+# Python зависимости
+pip install --upgrade pip
 pip install -r requirements.txt
